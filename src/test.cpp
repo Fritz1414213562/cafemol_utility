c2d584cee6ebc509b55dacefb06bbdc65683b772
fritz <fritz1414213562@gmail.com>
2019-12-28 16:17:27 +0900

    replace: for DCDWriter

---
+++ src/test.cpp
@@ -0,0 +1,27 @@
+#include<DCD/DCDWriter.hpp>
+#include<ErrorMessage.hpp>
+#include<iostream>
+#include<string>
+#include<memory>
+
+
+int main(int argc, char *argv[]) {
+
+	cafemol::error_handling::Error_Output eout = cafemol::error_handling::Error_Output();
+	if (argc != 3) eout("too much or less arguments");
+	std::string inputfilename = argv[1];
+	std::string outputfilename = argv[2];
+
+	int i_start = 0;
+	int nstep_save = 1000;
+	int n_tstep = 100000;
+	int n_units = 18;
+	float delta = 0.3;
+	int ver = 24;
+
+	std::unique_ptr<cafemol::DCDWriter> converter = std::make_unique<cafemol::DCDWriter>(outputfilename, inputfilename);
+
+	converter->copy_DCD(i_start, nstep_save, n_tstep, n_units, delta, ver);
+
+	return 0;
+}
