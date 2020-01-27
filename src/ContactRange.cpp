9ac8bf7d7376d4fdc1bf58cf5dffb292610a88c7
fritz <fritz1414213562@gmail.com>
2020-01-27 19:56:56 +0900

    feat: Calculation of contact range

--- src/ContactRange.cpp
+++ src/ContactRange.cpp
@@ -60,16 +60,43 @@ int main(int argc, char *argv[]) {
 	const std::vector<std::size_t>& Protein_IDs = topology_reader->get_ProteinIDs();
 	sout(".... Done", "");
 
-	// contact search and output data
-	sout.output_HyphenBlock("Output the calculation results to " + output_name, BLOCK_SIZE);
+	// contact search 
 	sout("", "Searching contacted pairs along a DNA chain", "");
+	std::vector<std::array<int, 2>> protein_contact_FwRv_ends;
+	std::vector<int> dna_contact_atom_Fw_ends;
+	std::vector<int> dna_contact_atom_Rv_ends;
+	for (const std::array<std::vector<float>, 3>& snapshot : dcd_trajectory) {
+		const std::array<std::array<int, 2>, 2>& contact_pairs = contact_search->get_ContactEnds(DNA_IDs, Protein_IDs, snapshot);
+		protein_contact_FwRv_ends.push_back({contact_pairs[0][1], contact_pairs[1][1]});
+		dna_contact_atom_Fw_ends.push_back(contact_pairs[0][0]);
+		dna_contact_atom_Rv_ends.push_back(contact_pairs[1][0]);
+	}
+	sout(".... Done");
+
+	// convert atom_id of DNA to dsDNA bp of DNA
+	sout("Converting the Atom ID to the corresponding Residue ID");
+	const std::vector<int>& dna_contact_resi_Fw_ends = topology_reader->convert_DNAID2dsDNAResi(dna_contact_atom_Fw_ends);
+	const std::vector<int>& dna_contact_resi_Rv_ends = topology_reader->convert_DNAID2dsDNAResi(dna_contact_atom_Rv_ends);
+	sout(".... Done", "", "Calculation ends");
+	if ((protein_contact_FwRv_ends.size() != dna_contact_resi_Fw_ends.size()) ||
+		(protein_contact_FwRv_ends.size() != dna_contact_resi_Rv_ends.size())) {
+		eout("Frame size is not consistent among protein, dna residue ends");
+	}
+
+	// output results to file 'output_name'
+	const std::size_t& frame_size = protein_contact_FwRv_ends.size();
+
+	sout.output_HyphenBlock("Output the calculation results to " + output_name, BLOCK_SIZE);
 	std::ofstream ofs(output_name, std::ios::out);
 
 	ofs << "DNA_ID(fw) PROTEIN_ID(fw) DNA_ID(rv) PROTEIN_ID(rv)" << std::endl;
-	for (const std::array<std::vector<float>, 3>& snapshot : dcd_trajectory) {
-		const std::array<std::array<int, 2>, 2>& contact_pairs = contact_search->get_ContactEnds(DNA_IDs, Protein_IDs, snapshot);
-		ofs << contact_pairs[0][0] << " " << contact_pairs[0][1] << " " << contact_pairs[1][0] << " " << contact_pairs[1][1] << std::endl;
+	
+	for (std::size_t iframe = 0; iframe < frame_size; ++iframe) {
+		ofs << dna_contact_resi_Fw_ends[iframe] << " " << protein_contact_FwRv_ends[iframe][0] << " "
+			<< dna_contact_resi_Rv_ends[iframe] << " " << protein_contact_FwRv_ends[iframe][1] 
+			<< std::endl;
 	}
+
 	ofs.close();
 
 	sout("Program finished", "");
